298c5a7c80ab395bbdc33b4cfb6874478aaec381
xiangxisheng <xxs@firadio.com>
2018-05-17 13:19:14 +0800

    sql

--- MyProcHttpd.go
+++ MyProcHttpd.go
@@ -5,7 +5,7 @@ go get -u github.com/go-sql-driver/mysql
 package main
 import (
     "os"
-    //"bytes"
+    "bytes"
     "strconv"
     "fmt"
     "io"
@@ -45,9 +45,16 @@ func main() {
     checkErr(err)
 }
 func GetParamData()Table{
-    sqlstr := "SELECT t1.SPECIFIC_SCHEMA,t1.SPECIFIC_NAME,t1.ORDINAL_POSITION,t1.PARAMETER_NAME,t1.DATA_TYPE,t1.CHARACTER_MAXIMUM_LENGTH,t1.NUMERIC_PRECISION FROM `information_schema`.`PARAMETERS` t1"
+    //sqlstr := "SELECT t1.SPECIFIC_SCHEMA,t1.SPECIFIC_NAME,t1.ORDINAL_POSITION,t1.PARAMETER_NAME,t1.DATA_TYPE,t1.CHARACTER_MAXIMUM_LENGTH,t1.NUMERIC_PRECISION FROM `information_schema`.`PARAMETERS` t1"
     //sqlstr := "SELECT t1.SPECIFIC_SCHEMA,t1.SPECIFIC_NAME FROM `information_schema`.`PARAMETERS` t1"
-    return GetTableBySql(sqlstr)
+    var buf bytes.Buffer
+    buf.WriteString("SELECT T.SPECIFIC_SCHEMA,T.SPECIFIC_NAME,MAX(PARAMETERS)PARAMETERS FROM (")
+    buf.WriteString("SELECT t1.SPECIFIC_SCHEMA,t1.SPECIFIC_NAME,GROUP_CONCAT(t1.PARAMETER_NAME,'|',t1.DATA_TYPE,'|',IFNULL(t1.CHARACTER_MAXIMUM_LENGTH,''),IFNULL(t1.NUMERIC_PRECISION,'')ORDER BY ORDINAL_POSITION ASC)PARAMETERS FROM `information_schema`.`PARAMETERS` t1 GROUP BY t1.SPECIFIC_SCHEMA,t1.SPECIFIC_NAME")
+    buf.WriteString(" UNION ")
+    buf.WriteString("SELECT t2.ROUTINE_SCHEMA,t2.ROUTINE_NAME,NULL FROM `information_schema`.`ROUTINES` t2 WHERE t2.ROUTINE_TYPE='PROCEDURE'")
+    buf.WriteString(")T GROUP BY T.SPECIFIC_SCHEMA,T.SPECIFIC_NAME ORDER BY T.SPECIFIC_NAME ASC")
+    //fmt.Printf(buf.String())
+    return GetTableBySql(buf.String())
 }
 func GetProcData(procName string) Table {
     sqlstr := "CALL `" + procName + "`()"
